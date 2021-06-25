91f0d9e7ed665c6a23617100ad258b2ce212e0da
svarvel <varvello@gmail.com>
2021-06-25 12:02:11 -0400

    modified alexa

--- android/tester.sh
+++ android/tester.sh
@@ -134,35 +134,40 @@ if [ $app == "wyze" ]
 then 
 	echo "[$0][$app][`date +%s`] Getting access to video feed" 
 	tap_screen 540 719
-	sleep $duration 
+	sleep $duration
 	echo "[$0][$app][`date +%s`] Getting out of video feed" 
 	tap_screen 60 167
 elif [ $app == "smartlife" ]  
 then
 	echo "[$0][$app][`date +%s`] Turning smart plug either ON or OFF"
 	tap_screen 937 651
-	sleep $duration 
+	sleep $duration
 elif [ $app == "alexa" ] 
 then 	
-	echo "[$0][$app][`date +%s`] Turning music on" 
-	tap_screen 543 2080 1
-	tap_screen 209 491 1
-	tap_screen 540 2016 1
-	sleep $duration 
-	echo "[$0][$app][`date +%s`] Turning music off" 
-	tap_screen 880 1960 1
+	#echo "[$0][$app][`date +%s`] Turning music on" 
+	#tap_screen 543 2080 1
+	#tap_screen 209 491 1
+	#tap_screen 540 2016 1
+	#sleep $duration
+	#echo "[$0][$app][`date +%s`] Turning music off" 
+	#tap_screen 880 1960 1
+	echo "[$0][$app][`date +%s`] Sending aler" 
+	tap_screen 550 1150 1
+	adb -s $device_id shell input text "hello" 
+	tap_screen 535 1060
+	sleep $duration
 elif [ $app == "google" ] 
 then 	
 	echo "[$0][$app][`date +%s`] Turning music on" 
 	tap_screen 280 1414
-	sleep $duration 
+	sleep $duration
 	echo "[$0][$app][`date +%s`] Turning music on" 
 	tap_screen 280 1414
 elif [ $app == "smartthings" ] 
 then
 	echo "[$0][$app][`date +%s`] Turning smart plug either ON or OFF"
 	tap_screen 454 1375
-	sleep $duration 
+	sleep $duration
 fi 
 
 # close all pending apps
